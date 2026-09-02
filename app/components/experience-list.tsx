b01e59bcab5360ffc6ae9c1a61a96518a151cf07
lsahatqija <leke.sahatqija@live.com>
2026-09-02 16:36:14 +0200

    tech tag icons

--- app/components/experience-list.tsx
+++ app/components/experience-list.tsx
@@ -1,6 +1,58 @@
 import { ArrowUpRight, ChevronDown } from "lucide-react";
 import type { Experience } from "~/content/site";
 
+import androidIcon from "../../dark-graphic-kit/icons/mono/android.svg";
+import appleIcon from "../../dark-graphic-kit/icons/mono/apple.svg";
+import automotiveIcon from "../../dark-graphic-kit/icons/mono/automotive.svg";
+import blenderIcon from "../../dark-graphic-kit/icons/mono/blender.svg";
+import csharpIcon from "../../dark-graphic-kit/icons/mono/csharp.svg";
+import dockerIcon from "../../dark-graphic-kit/icons/mono/docker.svg";
+import expressIcon from "../../dark-graphic-kit/icons/mono/express.svg";
+import githubIcon from "../../dark-graphic-kit/icons/mono/github.svg";
+import html5Icon from "../../dark-graphic-kit/icons/mono/html5.svg";
+import javascriptIcon from "../../dark-graphic-kit/icons/mono/javascript.svg";
+import linuxIcon from "../../dark-graphic-kit/icons/mono/linux.svg";
+import mongodbIcon from "../../dark-graphic-kit/icons/mono/mongodb.svg";
+import nodejsIcon from "../../dark-graphic-kit/icons/mono/nodejs.svg";
+import oculusIcon from "../../dark-graphic-kit/icons/mono/oculus.svg";
+import oracleIcon from "../../dark-graphic-kit/icons/mono/oracle.svg";
+import phpIcon from "../../dark-graphic-kit/icons/mono/php.svg";
+import pythonIcon from "../../dark-graphic-kit/icons/mono/python.svg";
+import reactIcon from "../../dark-graphic-kit/icons/mono/react.svg";
+import steamIcon from "../../dark-graphic-kit/icons/mono/steam.svg";
+import typescriptIcon from "../../dark-graphic-kit/icons/mono/typescript.svg";
+import unityIcon from "../../dark-graphic-kit/icons/mono/unity.svg";
+import unrealEngineIcon from "../../dark-graphic-kit/icons/mono/unrealengine.svg";
+import vrIcon from "../../dark-graphic-kit/icons/mono/vr.svg";
+
+const techIcons: Record<string, string> = {
+  Android: androidIcon,
+  "Augmented Reality": vrIcon,
+  "Automotive Software": automotiveIcon,
+  Blender: blenderIcon,
+  "C#": csharpIcon,
+  Docker: dockerIcon,
+  Express: expressIcon,
+  "GitHub Actions": githubIcon,
+  iOS: appleIcon,
+  JavaScript: javascriptIcon,
+  Linux: linuxIcon,
+  "Mixed Reality": vrIcon,
+  MongoDB: mongodbIcon,
+  "Node.js": nodejsIcon,
+  Oculus: oculusIcon,
+  "Oracle Database": oracleIcon,
+  PHP: phpIcon,
+  Python: pythonIcon,
+  React: reactIcon,
+  Steam: steamIcon,
+  TypeScript: typescriptIcon,
+  Unity: unityIcon,
+  "Unreal Engine": unrealEngineIcon,
+  VR: vrIcon,
+  WebGL: html5Icon,
+};
+
 export function ExperienceList({ experiences }: { experiences: Experience[] }) {
   return (
     <div className="timeline" aria-label="Experience and external projects">
@@ -16,6 +68,9 @@ export function ExperienceList({ experiences }: { experiences: Experience[] }) {
               <span className="tech-list" aria-label="Technologies">
                 {experience.tech.map((tech) => (
                   <span className="badge" key={tech}>
+                    {techIcons[tech] && (
+                      <img className="badge-icon" src={techIcons[tech]} alt="" aria-hidden="true" />
+                    )}
                     {tech}
                   </span>
                 ))}
